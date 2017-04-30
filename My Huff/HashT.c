fce96c4a5fbbb20c5d0c0f289108c906e6cba390
Leonardo Leite <leo.leite@hotmail.com>
2017-04-29 22:55:24 -0300

    Add files via upload

---
+++ My Huff/HashT.c
@@ -0,0 +1,71 @@
+#include <string.h>
+#include "huff.c"
+#include "util.c"
+#define TAM_HASH 256
+
+typedef struct element
+{
+	unsigned char letter;
+	int freq;
+	char bitstring[30];
+}Element;
+
+typedef struct hash_table
+{
+	Element *table[TAM_HASH];
+}HashT;
+
+//cria hash
+HashT *create_hash()
+{
+	int i;
+	HashT *ht = (HashT*) malloc(sizeof(HashT));
+	for (i = 0; i < TAM_HASH; ++i)
+	{
+		ht -> table[i] = (Element*) malloc(sizeof(Element));
+		ht -> table[i] -> letter = i;
+		ht -> table[i] -> freq = 0;
+	}
+	return ht;
+}
+
+//copia a string do bit para a hash
+void addCharBits(HashT *ht, unsigned char c, char *bits)
+{
+	strcpy(ht -> table[(int)c] -> bitstring, bits);
+}
+
+//retorna o bit que representa esse caracter
+char *getCharBits(HashT *ht, unsigned char c)
+{
+	return ht -> table[(int)c] -> bitstring;
+}
+
+/*PECORRE A ARVORE E CRIA STRING COM O CAMINHO DE CADA LETRA; adicionando sempre na hash
+ *DIREITA 1, ESQUERDA 0;
+ *tem o esquema de corta o ultimo bit quando chamar a recussão(não to me lembrando bem)
+ */
+void preench_bit(Node *huff, HashT *ht)
+{
+	char bit[30];
+	int i;
+	for(i = 0; i < 30; ++i)//SETANDO TUDO COM CARACTER NULO; PRA NAO TER PROBLEMA COM LIXO;
+		bit[i] = '\0';
+
+	if(huff != NULL)
+	{
+		if(is_leaf(huff))
+		{
+			addCharBits(ht, huff->letter, bit);
+		}else
+		{
+			int tam_bit = strlen(bit);
+			bit[tam_bit] = '0';
+			bit[tam_bit + 1] = '\0';
+			preench_bit(huff -> left, ht);
+			bit[tam_bit] = '1';
+			bit[tam_bit + 1] = '\0';
+			preench_bit(huff -> right, ht);
+		}
+	}
+}
---
+++ My Huff/compres.c
@@ -0,0 +1,123 @@
+#include "huff.c"
+#include "util.c"
+#include "HashT.c"
+
+void freq_count(FILE* file, HashT *ht)
+{
+	unsigned int c;
+	while(c = gets(file) != EOF)
+	{
+		ht -> table[c] -> freq++;
+	}
+}//conta a frequencia e ja manda pra hash
+
+int escape(Node* huff, int escapes)
+{
+	if(huff != NULL)
+	{
+    	if((huff -> letter == 42 || huff -> letter == '\\') && is_leaf(huff))
+    		++escapes;
+		escapes = escape(huff -> left, escapes);
+		escapes = escape(huff -> right, escapes);
+	} 
+  return escapes;
+}
+
+void print_tree_header(FILE* file, Node* huff)
+{
+	if(huff != NULL)
+	{
+    	if((huff -> letter == 42 || huff -> letter == '\\') && is_leaf(huff))
+    	{
+    	  fprintf(file, "\\%c", huff -> letter);
+    	}else 
+    	{
+    	  fprintf(file, "%c", huff -> letter);
+    	}
+		print_tree_header(file, huff -> left);
+		print_tree_header(file, huff -> right);
+	}
+}//trata o caso dos * e do \\ 
+
+int escreverArquivoCompactado(FILE *arquivoCompactar, FILE *arquivoCompactado, HashT* ht)
+{
+	unsigned char byteLido;
+	unsigned char byte = 0;
+	char *bitshuff;
+	int bit_index = 7;
+	int lixo, i;
+	rewind(arquivoCompactar);
+	while(fscanf(arquivoCompactar, "%c", &byteLido) > 0)
+	{
+		bitshuff = getCharBits(ht, byteLido);//retorna o bit que representa esse caracter
+		for(i = 0 ; i < strlen(bitshuff); ++i)
+		{
+			if(bitshuff[i] != '0')
+				byte = set_bit(byte, bit_index);
+			bit_index--;
+			if(bit_index < 0)
+			{
+				bit_index = 7;
+				fprintf(arquivoCompactado, "%c", byte);
+				byte = 0;
+			}
+		}
+	}
+	lixo = bit_index + 1;
+	if(lixo == 8)
+		lixo = 0;
+	else
+		fprintf(arquivoCompactado, "%c", byte);
+
+	return lixo;
+}//RETORNA O TAMANHO DO LIXO
+
+
+void compress(char* file_name)
+{
+	FILE *old_file, new_file;
+	Node *queue = create_empty_queue();
+	HashT *hasht = create_hash();
+	Node* huffman_tree = create_empty_tree();//---inicialização
+
+	old_file = fopen(file_name, "rb");
+	new_file = fopen("new_file.huff", "wb");
+
+	freq_count(old_file, hasht);//eu abro e fecho meu arquivo nessa função tem algum problema?????????????????????
+	
+	int i;
+	for(i = 0; i < 256; i++)
+	{
+		if(table[i] -> freq > 0)
+			queue = create_node(queue, i, table[i] -> freq);
+	}//Inserindo todos os elementos que aparecem pelo menos uma vez na fila de prioridade
+
+	huffman_tree = huff_tree(queue);//transformando fila em arvore
+	fprintf(new_file, "00");//ocupando os dois primeiros bytes para guarda lugar pro lixo e size_tree
+	preench_bit(huffman_tree, hasht);//criando as strings 1 e 0
+	unsigned int tree_size = size_huff(huffman_tree) + scape(huffman_tree, 0);//pegando o tamanho da arvore
+	print_tree_header(file, huffman_tree);//tratando os * e \\
+
+	char *tree_header_tam = (char*)malloc(13*sizeof(char));
+	int_bina(tree_header_tam, tree_size, 13);//transformando o tamanho da arvore em binario
+
+	unsigned int lixo = escreverArquivoCompactado(old_file, new_file, ht);//pegando o tamanho do lixo
+
+	char *qtdLixo = (char*)malloc(4*sizeof(char));
+	int_bina(qtdLixo, lixo, 3);
+	qtdLixo[3] = '\0';//transformando o tamanho da lixo em binario
+
+	//montando o cabeçario
+	char header[17] = "";
+	strcpy(header, qtdLixo);
+	header[3] = '\0';
+	strcat(header, tree_header_tam);
+	header[16] = '\0';
+
+	rewind(new_file);
+	escreverBitsArquivo(new_file, header, 16);//coloca o header no incio do arquivo
+
+	printf("\nCompactação Finalizada!!");
+	fclose(old_file);
+	fclose(new_file);
+}
